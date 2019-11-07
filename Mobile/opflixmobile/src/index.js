4e031415604715261719253eb843ddf7844c8da7
hcdsfigueira@gmail.com <oo3pGt2/3tM7$I&;^Ue7$Qi,Y>
2019-11-07 17:30:26 -0300

    Finalizou a tela de login. Teste-a

--- Mobile/opflixmobile/src/index.js
+++ Mobile/opflixmobile/src/index.js
@@ -1,2 +1,43 @@
+//Dependencies
 import React from 'react';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+//Screens
+import HomeScreen from './pages/home';
+import LoginScreen from './pages/login';
 
+
+const AuthStack = createStackNavigator({
+    Login : {screen : LoginScreen}
+});
+
+const MainNavigation = createBottomTabNavigator({
+    Home: {screen : HomeScreen},
+},
+    {
+        initialRouteName: 'Home',
+        tabBarOptions: {
+            showLabel: true,
+            showIcon: true,
+            inactiveBackgroundColor: '#d9d2e4',
+            activeBackgroundColor: '#ffffff',
+            style: {
+                width: '100%',
+                height: 50
+            }
+        }
+    }
+);
+
+export default createAppContainer(
+    createSwitchNavigator(
+      {
+        MainNavigation,
+        AuthStack,
+      },
+      {
+        initialRouteName: 'AuthStack',
+      },
+    ),
+  );
--- Mobile/opflixmobile/src/pages/login.js
+++ Mobile/opflixmobile/src/pages/login.js
@@ -1,11 +1,90 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import {
+    Text,
+    TextInput,
+    View,
+    TouchableOpacity,
+    AsyncStorage,
+    Image,
+    Button,
+    StyleSheet
+} from 'react-native';
+import OpflixLogo from '../assets/img/logoEscuro.png';
 
-export default class Login extends Component{
-    render(){
-        return(
-            <View>
-                
+
+export default class Login extends Component {
+    static navigationOptions = {
+        header: null,
+    };
+
+    constructor() {
+        super();
+        this.state = {
+            userEmail: "",
+            userPassword: ""
+        }
+    }
+
+    _redirectToHome = async (token) =>{
+        if(token != null){
+            try{
+                await AsyncStorage.setItem('@opflix:token', token);
+                this.props.navigation.Navigate('MainNavigation');
+            }
+            catch(error){
+                console.warn(error);
+            }
+        }
+    }
+
+    _signIn = async () => {
+        fetch('http://localhost:5000/api/Usuarios/Login', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: this.state.userEmail,
+                senha: this.state.userPassword
+            })
+        })
+        .then(response => response.json())
+        .then(data => this._redirectToHome(data.token))
+        .catch(error => console.warn(error))
+    }
+
+    render() {
+        return (
+            <View style={styles.pageBackground}>
+                <Image style={styles.opflixLogo} source={OpflixLogo} />
+                <TextInput
+                    placeholder="Email"
+                    onChangeText={userEmail => this.setState({ userEmail })}
+                    value={this.state.userEmail}
+                />
+                <TextInput
+                    placeholder="Senha"
+                    onChangeText={userPassword => this.setState({ userPassword })}
+                    value={this.state.userPassword}
+                />
+                <TouchableOpacity onPress={this._signIn}>
+                    <Text>Entrar</Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    pageBackground: {
+        backgroundColor: '#2e2e2e',
+        height: '100%',
+    },
+    opflixLogo: {
+        height: 80,
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+
+});
